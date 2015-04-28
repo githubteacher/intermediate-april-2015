2578ec79dd83cddb98285632848cdc35c060b8c9
githubteacher <trainingdemos+githubteacher@github.com>
2015-04-28 15:28:45 -0700

    implement GetArgs fcn

--- IO.cc
+++ IO.cc
@@ -10,7 +10,10 @@ using namespace std;
 
     vector<string> GetArgs(int argc, char* argv[])
     {
-
+        vector<string> args;
+        for (int i = 0; i < argc; ++i)
+            args.push_back(argv[i]);
+        return args;
     }
 
 
